98c2ff9ddfbc9b9d8491568c84fcc63fe4bd913a
Mykyta Opaniuk <mykyta@opanyuk.com>
2020-05-02 16:25:46 +0300

    scripts: Add scripts for init yocto project
    Signed-off-by: Mykyta Opaniuk <mykyta@opanyuk.com>

---
+++ scripts/bbb_config.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ponyfyer() {
+    local search="\${HOME}"
+    local replace=$1
+    # Note the double quotes
+    sed -i "s/${search}/${replace}/g" conf/bblayers.conf
+}
+
+if [ -z "$1" ]
+then
+        echo "Dir path for yocto project is not set."
+        echo "Using: ./bbb_config.sh <dir_path>"
+        exit 1
+else
+        echo "yocto project dir path: " $1
+fi
+
+mkdir -p $1/bbb/build/conf
+# Set env for bitbake command
+cd $1/bbb/build
+source ../../poky-zeus/oe-init-build-env .
+cp ../../meta-bbb/conf/local.conf.sample conf/local.conf
+cp ../../meta-bbb/conf/bblayers.conf.sample conf/bblayers.conf
+
+ponyfyer
